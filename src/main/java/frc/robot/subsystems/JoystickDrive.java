b2aa476b4f5ce087acc32488ca6ff74075e00831
Emma <emmalivne.03@gmail.com>
2022-09-06 18:16:44 +0300

    add constructor and override methods

--- src/main/java/frc/robot/subsystems/JoystickDrive.java
+++ src/main/java/frc/robot/subsystems/JoystickDrive.java
@@ -1,4 +1,28 @@
 package frc.robot.subsystems;
 
-public class JoystickDrive {
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class JoystickDrive extends CommandBase {
+    public static Joystick joystick1;
+    public static Joystick joystick2;
+    public static Tank tank;
+
+    public JoystickDrive(Joystick joystick1, Joystick joystick2, Tank tank){
+        this.joystick1=joystick1;
+        this.joystick2=joystick2;
+        this.tank=tank;
+        addRequirements(tank);
+    }
+
+    @Override
+    public void execute() {
+        tank.drive(joystick1.getX(), joystick2.getY());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Tank.mainLeftMotor.stopMotor();
+        Tank.mainRightMotor.stopMotor();
+    }
 }
