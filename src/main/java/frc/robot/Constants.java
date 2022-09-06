e044d0773cb2711a57348ef95bdb04c0dacf366a
Emma <emmalivne.03@gmail.com>
2022-09-06 17:01:36 +0300

    add constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,5 +1,11 @@
 package frc.robot;
 
+import com.ctre.phoenix.motorcontrol.InvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
 public class Constants {
-    public static final
+    public static final boolean ENABLE_VOLT_COMP = true;
+    public static final int CONFIG_VOLT_COMPSATURATION = 12;
+    public static final InvertType CLOCKWISE =;
+    public static final UnitModel TICKS_PER_M = 
 }
