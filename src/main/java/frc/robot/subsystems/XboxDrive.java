d6a76c5ed0ad575001b11ffdd0601e1c002d835d
Emma <emmalivne.03@gmail.com>
2022-09-06 18:23:58 +0300

    add Xbox drive command

--- src/main/java/frc/robot/subsystems/XboxDrive.java
+++ src/main/java/frc/robot/subsystems/XboxDrive.java
@@ -1,4 +1,26 @@
 package frc.robot.subsystems;
 
-public class XboxDrive {
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class XboxDrive extends CommandBase {
+    public static XboxController xboxController;
+    public static Tank tank;
+
+    public XboxDrive(XboxController xboxController, Tank tank){
+        this.xboxController=xboxController;
+        this.tank=tank;
+        addRequirements(tank);
+    }
+
+    @Override
+    public void execute() {
+        tank.drive(xboxController.getLeftX(), xboxController.getRightY());
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        Tank.mainRightMotor.stopMotor();
+        Tank.mainLeftMotor.stopMotor();
+    }
 }
