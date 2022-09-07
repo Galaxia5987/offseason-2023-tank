2d1f25ba5ced23cc771204c49c39b7ca0eaf9d8b
Emma <emmalivne.03@gmail.com>
2022-09-07 17:48:33 +0300

    add actual ports

--- src/main/java/frc/robot/Ports.java
+++ src/main/java/frc/robot/Ports.java
@@ -3,9 +3,9 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
 public class Ports {
-    public static final int MAIN_LEFT_MOTOR = 0;
-    public static final int AUX_LEFT_MOTOR = 0;
-    public static final int MAIN_RIGHT_MOTOR = 0;
-    public static final int AUX_RIGHT_MOTOR = 0;
+    public static final int MAIN_LEFT_MOTOR = 28;
+    public static final int AUX_LEFT_MOTOR = 22;
+    public static final int MAIN_RIGHT_MOTOR = 40;
+    public static final int AUX_RIGHT_MOTOR = 11;
 
 }
