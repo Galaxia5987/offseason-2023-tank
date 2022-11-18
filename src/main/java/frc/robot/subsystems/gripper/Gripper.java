2713e3b0ac185b1bec49a3112bc126ddc69c701f
Emma <emmalivne.03@gmail.com>
2022-11-18 11:27:54 +0200

    Add gripper constructor and getters and setters

--- src/main/java/frc/robot/subsystems/gripper/Gripper.java
+++ src/main/java/frc/robot/subsystems/gripper/Gripper.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.gripper;
 
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
@@ -11,5 +13,41 @@ public class Gripper extends SubsystemBase {
     private final UnitModel unitModel = new UnitModel(Constants.Gripper.TICKS_PER_ROTATION);
     private final TalonSRX gripperMotor = new TalonSRX(Ports.Gripper.GRIPPER_MOTOR);
     private final TalonSRX wristMotor = new TalonSRX(Ports.Gripper.WRIST_MOTOR);
+
+    private Gripper() {
+        gripperMotor.enableVoltageCompensation(Constants.Gripper.ENABLE_VOLT_COMP);
+        gripperMotor.configVoltageCompSaturation(Constants.Gripper.CONFIG_VOLT_COMP);
+        gripperMotor.setInverted(Constants.Gripper.CLOCKWISE);//check this
+        gripperMotor.setNeutralMode(NeutralMode.Coast);
+        wristMotor.enableVoltageCompensation(Constants.Gripper.ENABLE_VOLT_COMP);
+        wristMotor.configVoltageCompSaturation(Constants.Gripper.CONFIG_VOLT_COMP);
+        wristMotor.setInverted(Constants.Gripper.CLOCKWISE);//check this
+        wristMotor.setNeutralMode(NeutralMode.Brake);
+
+    }
+
+    public static Gripper getINSTANCE() {
+        if (INSTANCE==null){
+            INSTANCE = new Gripper();
+        }
+        return INSTANCE;
+    }
+
+    public double getWristPower(){
+        return wristMotor.getMotorOutputPercent();
+    }
+
+    public void setWristPower(double power){
+        wristMotor.set(TalonSRXControlMode.PercentOutput, power);
+    }
+
+    public double getGripperPower(){
+        return gripperMotor.getMotorOutputPercent();
+    }
+
+    public void setGripperPower(double power){
+        gripperMotor.set(TalonSRXControlMode.PercentOutput, power);
+    }
+
 
 }
