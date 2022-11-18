a5eeb50b7c001b86d44dd2e68f770deef1eb488f
Emma <emmalivne.03@gmail.com>
2022-11-18 11:46:24 +0200

    Add constructor and override methods to MoveWrist

--- src/main/java/frc/robot/subsystems/gripper/commands/MoveWrist.java
+++ src/main/java/frc/robot/subsystems/gripper/commands/MoveWrist.java
@@ -1,7 +1,25 @@
 package frc.robot.subsystems.gripper.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.gripper.Gripper;
 
-public class Wrist extends CommandBase {
+public class MoveWrist extends CommandBase {
+    private final Gripper gripper;
+    private final double power;
 
+    public MoveWrist(Gripper gripper, double power) {
+        this.gripper = gripper;
+        this.power = power;
+        addRequirements(gripper);
+    }
+
+    @Override
+    public void execute() {
+        gripper.setWristPower(power);
+    }
+
+    @Override
+    public void end(boolean interrupted) {
+        gripper.setWristPower(0);
+    }
 }
