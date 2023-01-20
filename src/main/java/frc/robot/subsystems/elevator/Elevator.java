8cdfc916805a3f2ec9f757fa0b093c28bb4f5760
Alon Schwierz <@1>
2023-01-20 13:33:37 +0200

    delete irrelevant functions and documantation

--- src/main/java/frc/robot/subsystems/elevator/Elevator.java
+++ src/main/java/frc/robot/subsystems/elevator/Elevator.java
@@ -68,126 +68,24 @@ public class Elevator extends SubsystemBase {
         PIDController.setP(0.2);
         PIDController.setI(0);
         PIDController.setD(0);
-        PIDController.setFF(0);
     }
 
-    /**
-     * Gets the position of the motor (used for debugging).
-     *
-     * @return the position of the motor. [m]
-     */
-    public double getPosition() {
-        return unitMan.toUnits(encoder.getPosition());
-    }
-
-    /**
-     * Gets the power of the motor.
-     *
-     * @return the power of the motor. [-1,1]
-     */
-    public double getPower() {
-        return motor.get();
-    }
-
-    /**
-     * Sets the power of the motor.
-     *
-     * @param power the power to set. [-1,1]
-     */
     public void setPower(double power) {
         motor.set(power);
     }
 
-    /**
-     * Sets the height of the elevator.
-     * Converts the height to a position of the motor and calls the position based function.
-     *
-     * @param height       is the height to move the elevator to. [m]
-     * @param timeInterval is the time passed between the last call of the function. [s]
-     */
-    public void setHeight(double height, double timeInterval) {
-        setpointHeight = height;
-        setPosition(unitMan.toTicks(height), timeInterval);
-    }
-
-    /**
-     * Gets the current height of the elevator using the position of the motor.
-     *
-     * @return the height of the elevator. [m]
-     */
-    public double getHeight() {
-        return unitMan.toUnits(getPosition());
-    }
-
-    /**
-     * Sets the motor to the desired position.
-     *
-     * @param position     is the position to set the motor to. [ticks]
-     * @param timeInterval is the time interval between this and the last call of the function. [s]
-     */
-    public void setPosition(double position, double timeInterval) {
-        setpointHeight = unitMan.toUnits(position);
-        encoder.setPosition(position);
-    }
-
-    /**
-     * Checks whether the elevator is at the last setpoint height.
-     *
-     * @param setpoint         is the setpoint to check. [m] or [ticks]
-     * @param tolerance        is the allowable interval of the setpoint. [m] or [ticks]
-     * @param setpointIsHeight whether the setpoint to check is height or motor position.
-     * @return whether the elevator is in the desired interval in the desired units.
-     */
-    public boolean atSetpoint(double setpoint, double tolerance, boolean setpointIsHeight) {
-        return Math.abs(setpointIsHeight ? setpoint : unitMan.toTicks(setpoint) - getHeight()) < tolerance;
-    }
-
-    /**
-     * Gets the current setpoint of the elevator.
-     *
-     * @return the setpoint height. [m]
-     */
-    public double getSetpointHeight() {
-        return setpointHeight;
-    }
-
-    /**
-     * Stops the elevator.
-     */
-    public void terminate() {
-        motor.set(0);
-    }
-
-    /**
-     * Gets the motor current.
-     *
-     * @return the current output of the motor. [A]
-     */
-//    //public double getCurrentOutput() {
-//        return motor.getSupplyCurrent();
-//    }
-
-    /**
-     * Resets the encoder of the elevator.
-     */
-//    public void resetEncoder() {
-//        motor.setSelectedSensorPosition(0);
-//    }
     @Override
     public void periodic() {
         configurePID();
-    }
-
-    public void teleopPeriodic() {
         if (leftStick.get()) {
-            // Here, we run PID control like normal, with a constant setpoint of 30in.
-            double pidOutput = PIDController. (m_encoder.getDistance(), Units.inchesToMeters(30));
+            double pidOutput = PIDController.calculate(encoder.getDistance(), Units.inchesToMeters(30));
             motor.setVoltage(pidOutput);
         } else {
             // Otherwise, we disable the motor.
             motor.set(0.0);
         }
     }
+
 
     @Override
     public void simulationPeriodic() {
